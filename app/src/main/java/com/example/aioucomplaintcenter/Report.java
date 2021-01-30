18a4567d6b9006643a4dbe186c5d06a9d25070d3
Abdul Rauf <open.org.pk@gmail.com>
2021-01-30 10:57:20 +0500

    chage activity after successfully sending query.

--- app/src/main/java/com/example/aioucomplaintcenter/Report.java
+++ app/src/main/java/com/example/aioucomplaintcenter/Report.java
@@ -1,5 +1,6 @@
 package com.example.aioucomplaintcenter;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -203,6 +204,8 @@ public class Report extends AppCompatActivity {
 		
 		Toast.makeText(this,
 				"Problem Repoted Successfully...", Toast.LENGTH_SHORT).show();
+		
+		startActivity(new Intent(getApplicationContext(),OperationSelection.class));
 		
 	}
 	
